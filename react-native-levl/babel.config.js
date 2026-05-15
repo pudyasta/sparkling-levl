4497bed4f73591c2dfd4c6e3237abee4389c2a79
Pudyasta Satria <pudyastasatria@gmail.com>
2026-05-15 18:01:58 +0700

    eas build

--- react-native-levl/babel.config.js
+++ react-native-levl/babel.config.js
@@ -1,9 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    plugins: [
+      'react-native-reanimated/plugin', // 👈 add this, must be last
     ],
   };
 };
